b326d3452f127c05c9f430ab344debc1e90f2f67
Ramesh <hanumanthappar32@gmail.com>
2018-10-12 00:27:25 +0530

    Initial Commit

---
+++ newMyfamilyTree/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newMyfamilyTree
+//
+//  Created by Ramesh H on 12/10/18.
+//  Copyright © 2018 Ramesh H. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
